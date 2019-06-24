6b03894e9909a7173b1f3dae9aeb1804ebb5a8c8
pfg <omegariisthebest@gmail.com>
2019-06-24 12:06:11 -0700

    Warning for shortcuts 3.0 shortcuts

---
+++ docs/precache-manifest.4d8a1de6533ea6e76b12b047d488a2f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4401d45d3544bdb8fd9a",
+    "url": "/static/css/main.0c9a4378.chunk.css"
+  },
+  {
+    "revision": "4401d45d3544bdb8fd9a",
+    "url": "/static/js/main.55f414c9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c556c3bb3c104543bbb1",
+    "url": "/static/css/2.f606517a.chunk.css"
+  },
+  {
+    "revision": "c556c3bb3c104543bbb1",
+    "url": "/static/js/2.834e0549.chunk.js"
+  },
+  {
+    "revision": "8f940346dd3087a1434c0f1a4ab8edb4",
+    "url": "/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5d48f29d49ace9eac96b370bb3706793.js"
+  "/precache-manifest.4d8a1de6533ea6e76b12b047d488a2f5.js"
 );
 
 workbox.clientsClaim();
